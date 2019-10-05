980ba2365a7b5decb6ee0581515a8c2695706f08
Chelp <achelpanovv@gmail.com>
2019-10-05 20:08:14 +0300

    working solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'signet-test';
+  message = `Sorry, it seems that you forgot to enter text.`;
+  sentencesToShow: string[][];
+  showXML: boolean;
+  showCSV: boolean;
+  maxSentenceLength: number;
+
+  private static splitTextToSentences(text: string): string[] {
+    return text
+      .replace(/\n | \s\s+/g, ' ')
+      .trim()
+      .replace(/[,"'\/#$%\^&\*;:{}=_`~<>()]|(\s)-(\s)/g, '')
+      .replace(/[!+|?+|.{2,}]/g, '.')
+      .split('.')
+      .filter(sentence => sentence);
+  }
+
+  private static splitSentencesToWords(sentences: string[]): string[][] {
+    return sentences.map(sentence => sentence
+      .trim()
+      .split(' ')
+      .filter(word => word)
+      .sort((a, b) => a.localeCompare(b)));
+  }
+
+  private prepareSentences(text: string): void {
+    const sentences: string[] = AppComponent.splitTextToSentences(text);
+    const sentencesByWords: string[][] = AppComponent.splitSentencesToWords(sentences);
+    this.sentencesToShow = sentencesByWords;
+    let maxSentenceLength = 0;
+
+    for (const sentence of sentencesByWords) {
+      if (sentence.length > maxSentenceLength) {
+        maxSentenceLength = sentence.length;
+      }
+    }
+
+    this.maxSentenceLength = maxSentenceLength;
+  }
+
+  toggleXML(text: string): void {
+    if (!text) {
+      alert(this.message);
+      return;
+    }
+
+    this.prepareSentences(text);
+
+    this.showXML = !this.showXML;
+  }
+
+  toggleCSV(text: string): void {
+    if (!text) {
+      alert(this.message);
+      return;
+    }
+
+    this.prepareSentences(text);
+
+    this.showCSV = !this.showCSV;
+  }
+
+  clearResult(): void {
+    this.sentencesToShow = [];
+    this.showCSV = false;
+    this.showXML = false;
+    this.maxSentenceLength = 0;
+  }
+
+  paste(e): void {
+    e.preventDefault();
+    const text = e.clipboardData.getData('text/plain');
+    document.execCommand('insertHTML', false, text);
+  }
 }
